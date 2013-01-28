ab30ce7677ac31ee0178b890f080b701f4c6ddc5
Christopher Williams <itschriswilliams@gmail.com>
2013-01-28 16:40:27 -0500

    init commit w.foundation and users scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstFoundation::Application.config.secret_token = 'aa34af3231915c93df9d7ecf73a7c40fc3e9ffd7b721820df3957ec84f97ef5ce5c72bf84cabd35467cf17893a3e89e0ae6f50d52978964056999903d3bf1996'
